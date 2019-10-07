616481b0ea6d0ac251e3e9afd04bf242005db0bd
yonizadi <yonigoel@gmail.com>
2019-10-07 14:27:09 +0300

    Add IEX API key

--- run_me.py
+++ run_me.py
@@ -2,13 +2,13 @@ from pandas_datareader import data
 from iexfinance.stocks import Stock
 from datetime import datetime
 from iexfinance.stocks import get_historical_data
+from iexfinance.stocks import get_historical_intraday
 
-
-print('hello')
-# a = Stock("AAPL", token="pk_e26954db0aca40eabd7947fe39d03281")
-# a.get_quote()
-# print(a.get_company_name())
-# print(a.get_historical_prices())
+IEX_TOKEN = 'pk_e26954db0aca40eabd7947fe39d03281'
+a = Stock("AAPL", token="pk_e26954db0aca40eabd7947fe39d03281")
+a.get_quote()
+print(a.get_company_name())
+print(a.get_historical_prices())
 #
 # start_date = '2005-01-01'
 # end_date = '2019-12-30'
@@ -17,6 +17,8 @@ print('hello')
 start = datetime(2016, 1, 1)
 end = datetime(2019, 1, 1)
 
-# User pandas_reader.data.DataReader to load the desired data. As simple as that.
-panel_data = data.DataReader('INPX', 'iex', start, end, api_key="pk_e26954db0aca40eabd7947fe39d03281")
-print(type(panel_data))
+# # User pandas_reader.data.DataReader to load the desired data. As simple as that.
+# panel_data = data.DataReader('INPX', 'iex', start, end, api_key="pk_e26954db0aca40eabd7947fe39d03281")
+# print(panel_data)
+# tsla = Stock('TSLA', token=IEX_TOKEN)
+# print(tsla.get_price())
