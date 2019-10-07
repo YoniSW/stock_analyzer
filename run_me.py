42ca09a91774840e227e5d3e78f7269c134988b5
yonizadi <yonigoel@gmail.com>
2019-10-07 14:46:04 +0300

    Stock additional data

--- run_me.py
+++ run_me.py
@@ -5,18 +5,27 @@ from iexfinance.stocks import get_historical_data
 from iexfinance.stocks import get_historical_intraday
 
 IEX_TOKEN = 'pk_e26954db0aca40eabd7947fe39d03281'
-a = Stock("AAPL", token=IEX_TOKEN)
-a.get_quote()
+a = Stock("BAC", token=IEX_TOKEN)
+print("#### Stock Official name ####")
 print(a.get_company_name())
+
+print("\n#### Stock Main Data ####")
+print(a.get_quote())
+
+print("\n#### Stock last dividend ####")
+print(a.get_dividends())
+
+print("\n#### Stock historical prices ####")
 print(a.get_historical_prices())
+
+
+
 #
 # start_date = '2005-01-01'
 # end_date = '2019-12-30'
 #
-
-start = datetime(2016, 1, 1)
+# start = datetime(2016, 1, 1)
 end = datetime(2019, 1, 1)
-
 # # User pandas_reader.data.DataReader to load the desired data. As simple as that.
 # panel_data = data.DataReader('INPX', 'iex', start, end, api_key="pk_e26954db0aca40eabd7947fe39d03281")
 # print(panel_data)
